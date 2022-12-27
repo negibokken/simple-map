1b12b649a137563089d3bef62c3ad46b18bc11b2
bokken <negibokken@gmail.com>
2022-12-27 16:07:49 +0900

    Add cookie utility

--- pages/api/location.js
+++ pages/api/location.js
@@ -1,12 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+//
+import cookie from 'cookie';
 
 export default function handler(req, res) {
-    if (!req.headers.cookie) {
+    const parsedCookie = cookie.parse(req.headers.cookie);
+
+    if (!parsedCookie || !parsedCookie.location) {
         return res.end(JSON.stringify(
             { location: { x: 1, y: 1 } }
         ));
     }
-    const params = req.headers.cookie.replace(" ", "").split(";");
+    const params = parsedCookie.location.split(";");
     const locationParam = params.find((param) => param.startsWith("location="))
     let [rawy, rawx] = locationParam.split("=")[1].split(":");
     let y = parseInt(rawy), x = parseInt(rawx);
