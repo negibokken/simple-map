ab6337b0838647c464d93952ce374ce5f0557c44
bokken <negibokken@gmail.com>
2022-12-28 10:24:52 +0900

    Add debug log

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export const getServerSideProps = ({ req, res }) => {
         res.setHeader("Set-Cookie", `location=1:1; ${env === 'development' ? "" : "SameSite=None; Secure; "}HttpOnly; Path=/; ${partitioned && env === 'production' ? "Partitioned;" : ""}`)
     } else {
         const cookies = cookie.parse(req.headers.cookie);
-        const url = new URL(req.headers.proto + req.headers.host + req.url);
+        // const url = new URL(req.headers.proto + req.headers.host + req.url);
         // console.log("params: ", url.searchParams);
         // const locationParam = cookies.location;
         // let [rawy, rawx] = locationParam.split(":");
@@ -46,8 +46,8 @@ export const getServerSideProps = ({ req, res }) => {
         // }
         // res.setHeader("Set-Cookie", `location=${y}:${x}; ${env === 'development' ? "" : "SameSite=None; Secure; "}HttpOnly; Path=/; ${partitioned && env === "production" ? "Partitioned;" : ""}`);
     }
-    const url = new URL(req.headers.proto + req.headers.host + req.url);
-    return { props: { x, y, headers: JSON.stringify(req.headers), url: req.url, op: url.searchParams.get("op") } }
+    // const url = new URL(req.headers.proto + req.headers.host + req.url);
+    return { props: { x, y, headers: req.headers, url: req.url } }
 }
 
 export default function Home({ x, y, headers, url, op }) {
@@ -88,7 +88,8 @@ export default function Home({ x, y, headers, url, op }) {
                 <div>
                     <h1>Simple Map App</h1>
                     <div>{url}</div>
-                    <div>{headers}</div>
+                    <div>{JSON.stringify(headers)}</div>
+                    <div>{headers.proto + headers.host + url}</div>
                     <div>{op}</div>
                     <form method="GET" action="/">
                         <button name="op" type="submit" value="up">↑</button>
